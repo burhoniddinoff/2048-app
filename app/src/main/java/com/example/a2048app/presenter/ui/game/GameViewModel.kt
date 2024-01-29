41aa8d85851e296d88d72d0ba1871d1e4e35d42e
Javokhir <javokhir.akramjonov@gmail.com>
2024-01-29 19:56:50 +0100

    used navigation with debounce

--- app/src/main/java/com/example/a2048app/presenter/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/a2048app/presenter/ui/game/GameViewModel.kt
@@ -3,8 +3,14 @@ package com.example.a2048app.presenter.ui.game
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavDirections
 import com.example.a2048app.domain.AppRepository
 import com.example.a2048app.domain.AppRepositoryImpl
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class GameViewModel : ViewModel() {
     private val repository: AppRepository = AppRepositoryImpl.getRepository()
@@ -18,6 +24,9 @@ class GameViewModel : ViewModel() {
 
     private val _bestScoreLiveData = MutableLiveData<Int>()
     val bestScoreLiveData: LiveData<Int> get() = _bestScoreLiveData
+
+    private val _navigation = MutableSharedFlow<NavDirections>()
+    val navigation = _navigation.asSharedFlow()
 
     fun loadData() {
         _matrixLiveData.value = repository.getMatrix()
@@ -52,6 +61,12 @@ class GameViewModel : ViewModel() {
 
     fun finish(): Boolean {
         return repository.finish()
+    }
+
+    fun openScreen(navDirections: NavDirections) {
+        viewModelScope.launch {
+            _navigation.emit(navDirections)
+        }
     }
 
 }
--- app/src/main/java/com/example/a2048app/presenter/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/a2048app/presenter/ui/home/HomeScreen.kt
@@ -7,12 +7,19 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.a2048app.R
 import com.example.a2048app.databinding.ScreenHomeBinding
 import com.example.a2048app.presenter.ui.game.GameViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.launch
+import java.time.Duration
 
+@OptIn(FlowPreview::class)
 class HomeScreen : Fragment(R.layout.screen_home) {
     private val binding by viewBinding(ScreenHomeBinding::bind)
     private val navController by lazy(LazyThreadSafetyMode.NONE) { findNavController() }
@@ -23,35 +30,57 @@ class HomeScreen : Fragment(R.layout.screen_home) {
         openResumeGame()
         openInfo()
         openRate()
+
+        handleNavigations()
     }
 
     private fun openNewGame() {
         binding.btnNewGame.setOnClickListener {
             viewModel.restartGame()
-            navController.navigate(HomeScreenDirections.actionHomeScreenToGameScreen())
+            viewModel.openScreen(HomeScreenDirections.actionHomeScreenToGameScreen())
         }
     }
 
     private fun openResumeGame() {
         binding.btnKeepGoing.setOnClickListener {
-            navController.navigate(HomeScreenDirections.actionHomeScreenToGameScreen())
+            viewModel.openScreen(HomeScreenDirections.actionHomeScreenToGameScreen())
         }
     }
 
     private fun openInfo() {
         binding.btnHowToPlay.setOnClickListener {
-            navController.navigate(HomeScreenDirections.actionHomeScreenToInfoScreen())
+            viewModel.openScreen(HomeScreenDirections.actionHomeScreenToInfoScreen())
         }
     }
 
     private fun openRate() {
         binding.btnRate.setOnClickListener {
             try {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${requireContext().packageName}")))
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=${requireContext().packageName}")
+                    )
+                )
             } catch (e: ActivityNotFoundException) {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=${requireContext().packageName}")))
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=${requireContext().packageName}")
+                    )
+                )
             }
         }
     }
 
+    private fun handleNavigations() {
+        lifecycleScope.launch {
+            viewModel
+                .navigation
+                .debounce(200)
+                .collect {
+                    navController.navigate(it)
+                }
+        }
+    }
 }
